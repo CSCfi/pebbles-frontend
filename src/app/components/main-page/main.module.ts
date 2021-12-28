74a07807548eb7f65b05b42f277dd32a160b2709
anakaji <ai.nakajima@csc.fi>
2021-12-28 14:43:02 +0200

    refactoring: erased unnecessary components
    - Erased unnecessary components below
      - main-workspace-folders.component.html
      - main-admin.component.html
    
    - Reorganized the order of the declaration list in the main.module.ts

--- src/app/components/main-page/main.module.ts
+++ src/app/components/main-page/main.module.ts
@@ -9,41 +9,37 @@ import { SharedModule } from 'src/app/components/shared/shared.module';
 import { MaterialModule } from 'src/app/material.module';
 // ---- Pipe
 import { DateDisplayPipe } from '../../pipes/date-display.pipe';
-import { MainAccountComponent } from './main-account/main-account.component';
-import { MainActiveSessionsComponent } from './main-active-sessions/main-active-sessions.component';
-import { MainAdminComponent } from './main-admin/main-admin.component';
-import { MainAnnouncementComponent } from './main-announcement/main-announcement.component';
 // ---- Content component
 import { MainCatalogComponent } from './main-catalog/main-catalog.component';
-import { MainContentHeaderComponent } from './main-content-header/main-content-header.component';
-import { MainContentStateComponent } from './main-content-state/main-content-state.component';
-import { MainApplicationItemFormComponent } from './main-application-item-form/main-application-item-form.component';
+import { MainMyWorkspacesComponent } from './main-my-workspaces/main-my-workspaces.component';
+import { MainWorkspaceOwnerComponent } from './main-workspace-owner/main-workspace-owner.component';
+import { MainActiveSessionsComponent } from './main-active-sessions/main-active-sessions.component';
+import { MainUsersComponent } from './main-users/main-users.component';
+import { MainSystemStatusComponent } from './main-status/main-system-status/main-system-status.component';
+import { MainStatisticsComponent } from './main-statistics/main-statistics.component';
+import { MainAnnouncementComponent } from './main-announcement/main-announcement.component';
+import { MainHelpComponent } from './main-help/main-help.component';
+import { MainAccountComponent } from './main-account/main-account.component';
 // ---- Application parts component
 import { MainApplicationItemComponent } from './main-application-item/main-application-item.component';
+import { MainApplicationItemFormComponent } from './main-application-item-form/main-application-item-form.component';
 import { MainApplicationWizardFormComponent } from './main-application-wizard-form/main-application-wizard-form.component';
-import { MainHelpComponent } from './main-help/main-help.component';
 import { MainSessionButtonComponent } from './main-session-button/main-session-button.component';
-import { MainJoinWorkspaceDialogComponent } from './main-join-workspace-dialog/main-join-workspace-dialog.component';
-import { MainMyWorkspacesComponent } from './main-my-workspaces/main-my-workspaces.component';
-import { MainNavComponent } from './main-nav/main-nav.component';
 // ---- Structural components
 import { MainPageComponent } from './main-page.component';
 import { MainRoutingModule } from './main-routing.module';
-// ---- Shared component
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { MainContentHeaderComponent } from './main-content-header/main-content-header.component';
 import { MainSearchBoxComponent } from './main-search-box/main-search-box.component';
-import { MainStatisticsComponent } from './main-statistics/main-statistics.component';
-import { MainSystemStatusComponent } from './main-status/main-system-status/main-system-status.component';
-import { MainUsersComponent } from './main-users/main-users.component';
-import { MainWorkspaceApplicationsComponent } from './main-workspace-applications/main-workspace-applications.component';
-import { MainWorkspaceFoldersComponent } from './main-workspace-folders/main-workspace-folders.component';
-import { MainWorkspaceFormComponent } from './main-workspace-form/main-workspace-form.component';
-import { MainWorkspaceItemDetailComponent } from './main-workspace-item-detail/main-workspace-item-detail.component';
+import { MainContentStateComponent } from './main-content-state/main-content-state.component';
 // ---- Workspace parts component
+import { MainWorkspaceItemDetailComponent } from './main-workspace-item-detail/main-workspace-item-detail.component';
+import { MainJoinWorkspaceDialogComponent } from './main-join-workspace-dialog/main-join-workspace-dialog.component';
+import { MainWorkspaceFormComponent } from './main-workspace-form/main-workspace-form.component';
+import { MainWorkspaceApplicationsComponent } from './main-workspace-applications/main-workspace-applications.component';
 import { MainWorkspaceItemComponent } from './main-workspace-item/main-workspace-item.component';
 import { MainWorkspaceMembersComponent } from './main-workspace-members/main-workspace-members.component';
-import { MainWorkspaceOwnerComponent } from './main-workspace-owner/main-workspace-owner.component';
 import { MainWorkspaceQuotaFormComponent } from './main-workspace-quota-form/main-workspace-quota-form.component';
-// import { MessageComponent } from 'src/app/components/shared/message/message.component';
 
 @NgModule({
   declarations: [
@@ -54,28 +50,26 @@ import { MainWorkspaceQuotaFormComponent } from './main-workspace-quota-form/mai
     MainCatalogComponent,
     MainMyWorkspacesComponent,
     MainWorkspaceOwnerComponent,
-    MainAdminComponent,
     MainActiveSessionsComponent,
     MainStatisticsComponent,
     MainHelpComponent,
     MainAccountComponent,
     MainAnnouncementComponent,
-    MainWorkspaceItemComponent,
     MainWorkspaceFormComponent,
-    MainWorkspaceMembersComponent,
-    MainWorkspaceFoldersComponent,
-    MainApplicationItemComponent,
-    MainApplicationWizardFormComponent,
-    MainApplicationItemFormComponent,
+    MainWorkspaceItemComponent,
     MainWorkspaceItemDetailComponent,
+    MainWorkspaceMembersComponent,
     MainWorkspaceApplicationsComponent,
+    MainApplicationItemComponent,
+    MainApplicationItemFormComponent,
+    MainApplicationWizardFormComponent,
     MainSessionButtonComponent,
     MainContentStateComponent,
     MainJoinWorkspaceDialogComponent,
     MainUsersComponent,
-    DateDisplayPipe,
     MainWorkspaceQuotaFormComponent,
-    MainSystemStatusComponent
+    MainSystemStatusComponent,
+    DateDisplayPipe
   ],
   imports: [
     CommonModule,
