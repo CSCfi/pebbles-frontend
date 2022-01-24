54b024570eeecf9128a3d772ac71f99c57e92706
tourunen <olli.tourunen@csc.fi>
2022-01-24 17:08:28 +0200

    fix dateDisplay pipe for day before yesterday
    dateDisplay would return nothing for day before yesterday if the date
    was still within 48h. Change the logic so that today and yesterday are
    special cases and the normal date is rendered for everything else.

--- src/app/pipes/date-display.pipe.ts
+++ src/app/pipes/date-display.pipe.ts
@@ -5,36 +5,35 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateDisplayPipe implements PipeTransform {
 
-  transform(timestamp: number|null, user_type?: string): string {
+  transform(timestamp: number | null, user_type?: string): string {
     if (!timestamp) {
       return '-';
     }
 
     const dateOfTimestamp = new Date(timestamp * 1000);
     const today = new Date();
-    function pad(n) {return n < 10 ? '0' + n : n; }
+
+    function pad(n) {
+      return n < 10 ? '0' + n : n;
+    }
 
     let displayDate: string;
-    // check if the time is closer than 48 hours and use human friendly notation
-    if ( today.getTime() - dateOfTimestamp.getTime() < 3600 * 24 * 2 * 1000) {
-      if ( dateOfTimestamp.getUTCDate() === today.getDate()) {
-        displayDate = 'Today: '
-          + pad(dateOfTimestamp.getUTCHours())
-          + ':' + pad(dateOfTimestamp.getUTCMinutes())
-          + ':' + pad(dateOfTimestamp.getUTCSeconds());
-      } else if ( today.getDate() - dateOfTimestamp.getUTCDate() === 1) {
-        displayDate = 'Yesterday: '
-          + pad(dateOfTimestamp.getUTCHours())
-          + ':' + pad(dateOfTimestamp.getUTCMinutes())
-          + ':' + pad(dateOfTimestamp.getUTCSeconds());
-      }
-    }
-    else {
+    // use human friendly notation with more detail for today and yesterday
+    if (dateOfTimestamp.getUTCDate() === today.getDate()) {
+      displayDate = 'Today: '
+        + pad(dateOfTimestamp.getUTCHours())
+        + ':' + pad(dateOfTimestamp.getUTCMinutes())
+        + ':' + pad(dateOfTimestamp.getUTCSeconds());
+    } else if (today.getDate() - dateOfTimestamp.getUTCDate() === 1) {
+      displayDate = 'Yesterday: '
+        + pad(dateOfTimestamp.getUTCHours())
+        + ':' + pad(dateOfTimestamp.getUTCMinutes())
+        + ':' + pad(dateOfTimestamp.getUTCSeconds());
+    } else {
       displayDate = pad(dateOfTimestamp.getFullYear())
         + '-' + pad(dateOfTimestamp.getMonth() + 1)
         + '-' + pad(dateOfTimestamp.getDate());
     }
-
     return displayDate;
   }
 }
