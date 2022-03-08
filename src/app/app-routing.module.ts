4b90f5e1d8987898502f978ec6a3a7d55bc6426e
anakaji <ai.nakajima@csc.fi>
2022-03-08 14:14:08 +0200

    wip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
       breadcrumbs: ['welcome']
     }
   },
-  { path: 'main', loadChildren: './components/main-page/main-routing.module'},
+  { path: 'main', loadChildren: () => import('./components/main-page/main-routing.module').then(m => m.default)},
   { path: 'session/:id', component: SessionPageComponent,
     data: {
       title: 'Session',
