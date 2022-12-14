1a1fea83a738be35a5b5769d88c4af4bd93d2982
anakaji <ai.nakajima@csc.fi>
2022-12-14 21:30:29 +0200

    Altered confusing 'message' classes
    There are too many *-message/message-* classes other than real 'message' component.
    
    Those are confusing so change to other naming.

--- src/app/models/notification.spec.ts
+++ src/app/models/notification.spec.ts
@@ -2,6 +2,6 @@ import { Notification, NotificationType } from './notification';
 
 describe('Notification', () => {
   it('should create an notification', () => {
-    expect(new Notification('0', 'message', 'Sep 11 2020', NotificationType.Notice)).toBeTruthy();
+    expect(new Notification('0', 'notefication test', 'Sep 11 2020', NotificationType.Notice)).toBeTruthy();
   });
 });
